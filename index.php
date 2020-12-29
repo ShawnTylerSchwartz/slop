c9597668dc5782033ed00554a8c10ea334cf8516
Shawn Schwartz <shawnschwartz@ucla.edu>
2020-12-29 09:15:48 -0800

    add in frontmatter

---
+++ index.php
@@ -0,0 +1,50 @@
+<!-- Shawn T. Schwartz, 2020 -->
+<!DOCTYPE html>
+<html>
+<head>
+    <script src="jsPsych/jspsych.js"></script>
+    <script src="jsPsych/plugins/jspsych-image-keyboard-response.js"></script>
+    <script src="jsPsych/plugins/jspsych-html-button-response.js"></script>
+    <script src="jsPsych/plugins/jspsych-html-keyboard-response.js"></script>
+    <script src="jsPsych/plugins/jspsych-instructions.js"></script>
+    <link rel="stylesheet" href="jsPsych/css/jspsych.css">
+    <style>
+        img {
+        width: 300px;
+        }
+
+        .wordgrid {
+            table-layout: fixed !important; 
+            white-space: nowrap !important;
+        }
+
+        .black {
+            font-size: 50px; 
+            font-weight: bold; 
+            color: black; 
+            text-transform: uppercase;
+        }
+
+        .colrd {
+            font-size: 50px; 
+            font-weight: bold; 
+            color: red; 
+            text-transform: uppercase;
+        }
+
+        .colbl {
+            font-size: 50px; 
+            font-weight: bold; 
+            color: blue; 
+            text-transform: uppercase;
+        }
+
+        #instructionTable td {
+            text-align: center; 
+            /* vertical-align: middle; */
+        }
+    </style>
+</head>
+
+<body>
+</body>
